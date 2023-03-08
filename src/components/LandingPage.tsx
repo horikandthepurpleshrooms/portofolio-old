c01f378288e23b82f029d991fea04d5269fd747d
Horik <h22ea@icloud.com>
2023-03-08 23:09:57 +0200

    integrate font

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import {
-  Box, Text,
+  Box, Heading,
 } from "@chakra-ui/react"
 import useWindowDimensions from "../hooks/useWindowDimensions"
 
@@ -9,8 +9,8 @@ const LandingPage = () => {
   const { height } = useWindowDimensions()
 
   return (
-    <Box height={(height / 1.2)} borderColor={"#252525"} borderWidth={1} m={10} display={"flex"} flex={1} justifyContent={"center"} alignItems={"center"} backgroundColor={"coral"}>
-      <Text color={"#235"} display={"flex"} justifyContent={"center"} fontSize={"2rem"}>{`HORIK`}</Text>
+    <Box height={(height / 1.2)} borderColor={"#252525"} borderWidth={1} m={10} display={"flex"} flex={1} justifyContent={"center"} alignItems={"center"} backgroundColor={"teal.100"}>
+      <Heading fontFamily={"Poppins, sans-serif;"} fontWeight={"thin"} color={"#235"} display={"flex"} justifyContent={"center"} fontSize={100} >{`HORIK`}</Heading>
     </Box>
   )
 }
