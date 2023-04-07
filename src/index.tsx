b3c60a265e1e5f56e086da81b5d93e9a27656f69
Horik <h22ea@icloud.com>
2023-04-07 22:21:40 +0300

    update index

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
-import { app } from './firebase/firebase'
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
-
 
 const container = document.getElementById("root")
 if (!container) throw new Error('Failed to find the root element')
 const root = ReactDOM.createRoot(container)
-let spores = app
 
 root.render(
   <React.StrictMode>
